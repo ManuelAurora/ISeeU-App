ec67bee15d80ba6d04dbe8cc2907dd7abfac6e40
Manuel <naruto06@mail.ru>
2016-03-20 07:45:04 +0300

    Initial Commit

---
+++ ISeeU-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ISeeU-App
+//
+//  Created by Мануэль on 20.03.16.
+//  Copyright © 2016 AuroraInterplay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
